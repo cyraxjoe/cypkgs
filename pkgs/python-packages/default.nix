7a03532b34376f8f4ac25c5b52d58ab94264ee66
Joel Rivera <rivera@joel.mx>
2017-03-09 02:36:18 -0600

    Remove old version of awake

--- pkgs/python-packages/default.nix
+++ pkgs/python-packages/default.nix
@@ -18,22 +18,6 @@ with pythonPackages;
     };
   };
   
-  awake_0_6_1 = buildPythonPackage rec {
-    name = "awake-${version}";
-    version = "0.6.1";    
-    doCheck = false;
-    src = fetchurl {
-      url = "https://pypi.python.org/packages/source/a/awake/${name}.tar.gz";
-      md5 = "cd54eeb47aadb2b989db1eb28d409e4b";
-    };
-    meta = {
-      license = lib.licenses.gpl3;    
-      maintainers = [ maintainer ];
-      platforms = lib.platforms.all;
-    };
-
-  };
-
   TA-Lib = buildPythonPackage rec {
     name = "TA-Lib-${version}";
     version = "0.4.10";
