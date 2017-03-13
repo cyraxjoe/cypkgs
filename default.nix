75584735e3573c8cd26367ad997f8167c30227d5
Joel Rivera <rivera@joel.mx>
2017-03-13 00:50:30 -0600

    Remove unnecesary rec for attrsets

--- default.nix
+++ default.nix
@@ -14,14 +14,13 @@ let
       pythonPackages = pp;
       cypkgs = self;
       inherit pkgs maintainer;
-     }; in
-     rec {
+     }; in {
        python27Packages = pythonPackagesWith pkgs.python27Packages;
        python34Packages = pythonPackagesWith pkgs.python34Packages; 
        python35Packages = pythonPackagesWith pkgs.python35Packages; 
          pythonPackages = pythonPackagesWith defaultPythonPackages;
      };
-  self = rec {
+  self = {
    tws-api = pkgs.callPackage ./pkgs/tws-api { inherit pkgs maintainer; };
    ta-lib = pkgs.callPackage ./pkgs/ta-lib { inherit pkgs maintainer; };
   } // pythonPkgs;
