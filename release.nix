537482694c84de44df29936ade818567539f64cb
Joel Rivera <rivera@joel.mx>
2017-10-06 11:12:27 -0500

    Add test release.nix

---
+++ release.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? <nixpkgs> }:
+let
+  pkgs = import nixpkgs {};
+  cypkgs = import ./default.nix { inherit nixpkgs; };
+  pythonPackages = with pkgs.lib;
+     zipLists [ "py27"  "py34" "py35" ]
+     [ cypkgs.python27Packages
+       cypkgs.python34Packages
+       cypkgs.python35Packages ];
+                                                                        
+   extra_packages = with pkgs.lib;
+      map(e:
+          let version = e.fst;
+              packages = e.snd;
+          in
+            mapAttrs(name: drv:
+              { "${version}-${name}" = drv; })
+              (filterAttrs (n: v: isDerivation v) packages)) pythonPackages;
+in
+{
+  ta-lib = cypkgs.ta-lib;
+  extra = extra_packages;
+  pp = pythonPackages;
+} 
+
